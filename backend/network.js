4e09207f76c7f2090ce3a691054834e0ab436281
intern-azamPay <intern1@azampay.com>
2021-09-11 13:15:59 -0400

    mongoose

--- backend/network.js
+++ backend/network.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://husseinfk:cleanairwalk@cluster0.wqacm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+
 function getTreeData(){
 }
 
