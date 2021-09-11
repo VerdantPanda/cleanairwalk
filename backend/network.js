91b79ff9fffe1f2f1f0b5e028dfc0a3dfb762b79
Ibrahim Fagbamila <ibrahim.fagbamila@aol.com>
2021-09-11 14:45:01 -0400

    Moved some stuff

--- backend/network.js
+++ backend/network.js
@@ -1,27 +1,3 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Pollutant = require('./models/pollutant');
-const app = express();
-
-//connect to mongodb
-const dbURI = 'mongodb+srv://husseinfk:cleanairwalk@cluster0.wqacm.mongodb.net/cleanairwalkdb?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(result => console.log('Connected to DB'))
-.catch(err => console.log(err));
-
-const pollutant = new Pollutant({
-    type: 'smokers',
-    coordinates: '2.5,3.9',
-    description: 'Saw 2 people smoking'
-});
-
-pollutant.save()
-    .then((result) => {
-        console.log(result)
-    })
-    .catch((err) => { 
-        console.log(err);
-    });
 
 
 
